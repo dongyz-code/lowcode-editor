b5a343a618547cf44dcdfcb573e14d185909d4e3
DongYuZhong <dongyzchn@gmail.com>
2022-05-14 10:29:44 +0800

    feat: 代码格式重构

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'semi': ['error', 'always'],
+    'semi': ['error', 'never'],
     'quotes': ['error', 'single'],
     'quote-props': ['error', 'consistent'],
   },
@@ -29,4 +29,4 @@ module.exports = {
       },
     },
   ],
-};
+}
